72f164bbdb0479d84e44a39a27f12b20ebce292d
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-10-18 13:16:02 -0700

    Done

--- task1.py
+++ task1.py
@@ -19,8 +19,12 @@ round(103.2221,2) -> 103.22
 """
 
 openBalance = 1000
-deposits = [200,200,200,200,200,200,200,200,200,200,200,200]            # final: 3328.8
-#deposits = [500,1000,300,-250,500,185,-500,205,1200,-550,125,200]      # final: 3887.71
-#deposits = [1000,542,221.52,983,29.75,-10,982.23,10988,287,198,32,100]  # final: 16702.93
+#deposits = [200,200,200,200,200,200,200,200,200,200,200,200]            # final: 3528.8
+#deposits = [500,1000,300,-250,500,185,-500,205,1200,-550,125,200]      # final: 4087.71
+deposits = [1000,542,221.52,983,29.75,-10,982.23,10988,287,198,32,100]  # final: 16802.93
 
-    
+for i in range(12):
+    Total=openBalance*1.005
+    Total=round(Total,2)
+    openBalance=Total+deposits[i]
+print(openBalance, 'is the amount of in the account at the end of the year.')
